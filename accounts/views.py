3d16fecdce3ffa981b4c574d61c7d31513a76bac
Mans-Alien <mans012033945420@gmail.com>
2023-08-08 20:28:00 +0300

    create a profile page and connect it to the database

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import SignupForm
+from django.contrib.auth import authenticate, login
+from .models import Profile
 
 # Create your views here.
-def signup(requtest):
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password2']
+            user = authenticate(username= username, password= password)
+            login(request, user)
+            return redirect("/accounts/profile")
+    else:
+        form = SignupForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile':profile})
+
+
+def profile_edit(request):
     pass
